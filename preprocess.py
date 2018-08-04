1e1db07941b01665d445f5eb4ab376c32826967d
akshay <akshayparanjape.iitd@gmail.com>
2018-08-04 21:15:00 +0200

    new functions in pre-processing

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,17 @@
 '''
 
-This file include some minor pre-processsin for the cifar 10 model
+This file include some minor pre-processsin for the cifar 10 model and few functions
+
+# Compute input images and labels for training. If you would like to run
+ inputs, labels = inputs()
+
+# Compute inference on the model inputs to make a prediction.
+ logits = inference(inputs)
+
+ # Compute the total loss of the prediction with respect to the labels.
+ loss = loss(logits, labels)
+
+ all this function will be calculated for the batch size
 
 '''
 
@@ -28,7 +39,8 @@ BATCH_SIZE = 64
 # TODO: what does weight decay do  ?
 
 # Model Constants
-
+MODEL = 'Cifar10_Conv_pool'
+MODEL_ARGS = (NUM_CLASSES, WEIGHT_DECAY_CONV, WEIGHT_DECAY_FULL)
 
 # FLAGS available in module
 # Flags available in all modules
@@ -38,3 +50,50 @@ tf.app.flags.DEFINE_integer('batch_size', BATCH_SIZE,
                             """Number of images to process in a batch.""")
 tf.app.flags.DEFINE_integer('num_epochs', NUM_EPOCHS,
                             """Number of training epochs.""")
+
+
+# get input in the form  (input,image)
+def input(eval_data, perform_shuffle):
+
+	num_epochs = FLAGS.num_epochs if not eval_data else None
+	data = dataset.get_dataset(eval_data,FLAGS.batch_size, num_epochs, perform_shuffle)
+	
+	# Create an iterator over the dataset
+    iterator = dataset.make_one_shot_iterator()
+    images, labels = iterator.get_next()
+    labels = tf.reshape(labels, [-1])  # Make sure labels is a 1-D tensor
+
+	return images,labels
+
+
+# prediction for the image according to the class, this will return the logits
+def inference(images, eval_data):
+	if hasattr(model,MODEL):
+		ANN = getattr(model, MODEL)
+	else:
+		raise ValueError('Model %s not present in cifar10_model' % MODEL)
+	ANN = ANN(*MODEL_ARGS) # construction of the class
+	return ANN(images,eval_data)
+
+
+# calculating the total loss with respect to the label, logits
+'''
+We can use mean squarred error
+Cross entropy loss
+'''
+# Loss function
+def loss(logits,labels):
+
+	labels = tf.cast(labels, tf.int64)
+	cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels,logits=logits,name = 'cross_entropy_per_example')
+	cross_entropy_mean = tf.reduce_mean(cross_entropy,name='cross_entropy')
+	return cross_entropy_mean
+
+
+
+'''
+cross entropy  mean  = 1/N * ()
+
+'''
+
+
